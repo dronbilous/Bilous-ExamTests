a1ab21e4b664e7546e790329ec28c0b1d1e0b363
dronbilous <andrii1981bilous@gmail.com>
2025-02-15 20:54:26 +0100

    structure Page Object

---
+++ src/main/java/org/pages/CommonActionsWithElement.java
@@ -0,0 +1,13 @@
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonActionsWithElement {
+    WebDriver webDriver;
+
+    public CommonActionsWithElement(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+
+}
---
+++ src/test/java/org/BaseTest/BaseTest.java
@@ -0,0 +1,34 @@
+package org.BaseTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.pages.PageProvider;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class BaseTest {
+    private WebDriver webDriver;
+    private Logger logger = Logger.getLogger(getClass());
+    protected PageProvider pageProvider;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("Browser was opened");
+        pageProvider = new PageProvider(webDriver);
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+        logger.info("Browser was closed");
+    }
+}
